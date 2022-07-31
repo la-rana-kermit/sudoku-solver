1e4873b5f2fc43300c99c56bcc45e99d098d0c8c
La rana Kermit <La.rana.kermit@protonmail.com>
2022-07-31 20:37:05 +0200

    Add cell class
    Added the Cell class with its attributes, initialization methods and methods printall(), add_candidate(), remove_candidate() and set_value()

---
+++ sudoku_solver/Cell.py
@@ -0,0 +1,46 @@
+class Cell:
+  
+    # ATTRIBUTES
+    value = None    # Value of the cell
+    x = None    # X coordinate of cell in sudoku grid
+    y = None    # Y coordinate of cell in sudoku grid
+    sub_x = None    # X coordinate of cell in its 3x3 subgrid
+    sub_y = None    # Y coordinate of cell in its 3x3 subgrid
+    subgrid_x = None    # X coordinate of cell 3x3 subgrid
+    subgrid_y = None    # Y coordinate of cell 3x3 subgrid
+    candidates = list() # List of all possible candidates
+
+
+    # METHODS
+    # Init methods
+    def __init__ (self, value, x, y):
+        self.value = value
+        self.x = x
+        self.y = y
+        self.sub_x = x % 3
+        self.sub_y = y % 3
+        self.subgrid_x = x // 3
+        self.subgrid_y = y // 3
+
+    # Print cell attributes
+    def printall(self):
+        print("Value = " + str(self.value) + "\nX = " + str(self.x) + "\nY = " + str(self.y) + "\nSub X = " + str(self.sub_x) + "\nSub Y = " + str(self.sub_y) + "\nSubgrid X = " + str(self.subgrid_x) + "\nSubgrid Y = " + str(self.subgrid_y))
+
+    
+    # Add candidate
+    def add_candidate(self, value):
+        if value not in self.candidates:    # Checks that the candidate is not already present       
+            self.candidates.append(value)
+            self.candidates.sort()
+
+    # Remove candidate
+    def remove_candidate(self, value):
+        if value in self.candidates:    # Check that the candidate exists
+            self.candidates.remove(value)
+            self.candidates.sort()
+
+    # Set a cell value
+    def set_value(self, value):
+        if self.value == None:  # Check that value is not already set
+            self.value = value
+            self.candidates.clear()
